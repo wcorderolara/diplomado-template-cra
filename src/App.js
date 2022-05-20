e618d4bca786f0aa2aa26b11bda971a611af9981
wcorderolara <wcordero.lara@gmail.com>
2022-05-19 20:01:58 -0600

    updating and setting the state un fc

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
       <div className="App">
         {/* <Profile printName={this.printName} /> */}
         {/* <Clock /> */}
-        <Friend src="https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip"/>
+        <Friend />
       </div>
     );
   }
--- src/features/Friend/Friend.js
+++ src/features/Friend/Friend.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const Friend = (props) => {
+    const [color, setColor] = useState('Tomato');
+
+    const divStyle = {backgroundColor: color}
+    
     return(
-        <div>
-            <img src={props.src} width="500"/>
+        <div className="Div" style={divStyle}>
+            <p>The selected color is: {color}</p>
+
+            <button onClick={() => setColor('Aquamarine')}>
+                Aquamarine
+            </button>
+            <button onClick={() => setColor('BlueViolet')}>
+                BlueViolet
+            </button>
+            <button onClick={() => setColor('Chartreuse')}>
+                Chartreuse
+            </button>
+            <button onClick={() => setColor('CornflowerBlue')}>
+                CornflowerBlue
+            </button>
+
         </div>
     )
 }
