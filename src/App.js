73368e0d485906c349dc8b02e57a26c9d3bdd878
wcorderolara <wcordero.lara@gmail.com>
2022-05-19 19:45:25 -0600

    adding props to function components in React

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import Profile from './features/Profile/Profile'
-import Clock from './features/Clock/Clock'
+// import Clock from './features/Clock/Clock'
+import { Friend } from './features/Friend/Friend'
 import './App.css';
 
 class App extends React.Component {
-
-  // printName() {
-  //   alert('Welcome to React and Node Bootcamp');
-  // }
 
   render() {
     return (
       <div className="App">
         {/* <Profile printName={this.printName} /> */}
-        <Clock />
+        {/* <Clock /> */}
+        <Friend src="https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip"/>
       </div>
     );
   }
---
+++ src/features/Friend/Friend.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Friend = (props) => {
+    return(
+        <div>
+            <img src={props.src} width="500"/>
+        </div>
+    )
+}
