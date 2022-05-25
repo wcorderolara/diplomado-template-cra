179bdef3d34549f8f3d3c24845ff0a5ff1686cb9
wcorderolara <wcordero.lara@gmail.com>
2022-05-24 20:22:17 -0600

    react router dom update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 // import logo from './logo.svg';
-// import Profile from './features/Profile/Profile'
-// import Clock from './features/Clock/Clock'
-// import { Friend } from './features/Friend/Friend'
-// import Effect from './features/Effect/Effect';
-import PicSearch from './features/PicSearch/PicSearch';
 import './App.css';
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>
-        {/* <Profile printName={this.printName} /> */}
-        {/* <Clock /> */}
-        {/* <Friend /> */}
-        <PicSearch />
-      </div>
+      <>
+        <div className="ui pointing menu inverted">
+          <div className="ui container">
+            <Link to="/" className="item"><i className="icon home"></i> Home</Link>
+            <Link to="/clock-app" className="item">Clock App</Link>
+            <Link to="/profile-app" className="item">Profile App</Link>
+            <Link to="/picsearch-app" className="item">Pic Search App</Link>
+            <Link to="/increment-app" className="item">Increment App</Link>
+          </div>
+        </div>
+        <div className="ui container">
+          <Outlet />
+        </div>
+      </>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Profile from './features/Profile/Profile'
+import Clock from './features/Clock/Clock'
+import { Friend } from './features/Friend/Friend'
+// import Effect from './features/Effect/Effect';
+import PicSearch from './features/PicSearch/PicSearch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} >
+          <Route path="clock-app" element={<Clock />} />
+          <Route path="picsearch-app" element={<PicSearch />} />
+          <Route path="profile-app" element={<Profile />} />
+          <Route path="increment-app" element={<Friend />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
